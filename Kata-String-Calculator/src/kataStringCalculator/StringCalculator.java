79f0dd97dc52f3778a406c9c289f316952d9cfe0
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-05-01 11:13:23 +0200

    GREY refactor

--- Kata-String-Calculator/src/kataStringCalculator/StringCalculator.java
+++ Kata-String-Calculator/src/kataStringCalculator/StringCalculator.java
@@ -3,10 +3,17 @@ package kataStringCalculator;
 public class StringCalculator {
 
 	public static int add(String string) {
-		if (string.isEmpty())
+		if (isEmpty(string))
 			return 0;
 		else
-			return Integer.parseInt(string);
+			return toInt(string);
 	}
 	
+	private static boolean isEmpty(String string) {
+		return string.isEmpty();
+	}
+
+	private static int toInt(String string) {
+		return Integer.parseInt(string);
+	}
 }
